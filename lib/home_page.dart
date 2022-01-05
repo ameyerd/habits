7886d72fc0c03dbd9740bed106cc700626081f1d
Amey Erdenebileg <ameyerdenebileg@Ameys-MacBook-Pro.local>
2022-01-04 18:44:38 -0700

    added habit tiles and add buttom modal sheet

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,45 +9,91 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List habitData = ['habit one', 'habit two', 'habit three'];
+
+  // void insertHabit() {
+  //   final newIndex = habitData.length;
+  //   const newHabit = 'habit four';
+  //
+  //   habitData.insert(newIndex, newHabit);
+  //   print("adding habit");
+  // }
+
+  Widget buildSheet() => Container();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: Container(
-          padding: const EdgeInsets.only(left: 20, right: 20),
-          child: Column(
-            children: [
-              // Habits Title
-              Row(
-                children: [
-                  Container(
-                    padding:
-                        const EdgeInsets.only(top: 70, left: 110, bottom: 10),
-                    child: Text(
-                      "habits",
-                      style: TextStyle(
-                          fontSize: 50,
-                          color: Colors.black,
-                          fontWeight: FontWeight.w800),
-                    ),
+      backgroundColor: Colors.white,
+      body: Container(
+        padding: const EdgeInsets.only(left: 20, right: 20),
+        child: Column(
+          children: [
+            // Habits Title
+            Row(
+              children: [
+                Container(
+                  padding:
+                      const EdgeInsets.only(top: 70, left: 110, bottom: 10),
+                  child: const Text(
+                    "habits",
+                    style: TextStyle(
+                        fontSize: 50,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w800),
                   ),
-                ],
+                ),
+              ],
+            ),
+            // Curved box container
+            Row(children: [
+              Container(
+                width: MediaQuery.of(context).size.width * 0.9,
+                height: 650,
+                decoration: BoxDecoration(
+                    color: const Color(0xffbed8ff),
+                    borderRadius: BorderRadius.circular(20.0)),
+                alignment: Alignment.center,
+                child: ListView.builder(
+                  itemCount: habitData.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Center(
+                        child: SizedBox(
+                          width: 3050,
+                          height: 80,
+                          child: Card(
+                            child: ListTile(
+                              title: Text('${habitData[index]}'),
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
               ),
-              // Curved box container
-              Row(
-                children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.9,
-                    height: 650,
-                    decoration: BoxDecoration(
-                        color: Color(0xffbed8ff),
-                        borderRadius: BorderRadius.circular(20.0)),
-                  ),
-                ],
-              ),
-              // Collapsable habit, expansion panel list
-            ],
+            ]),
+            // Collapsable habit, expansion panel list
+          ],
+        ),
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: SizedBox(
+          height: 60,
+          width: 60,
+          child: FloatingActionButton(
+            backgroundColor: const Color(0xff7b61ff),
+            onPressed: () => showModalBottomSheet(
+              context: context,
+              builder: (context) => buildSheet(),
+            ),
+            child: const Icon(Icons.add),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
